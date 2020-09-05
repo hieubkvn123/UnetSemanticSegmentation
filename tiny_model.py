bf8ac7060640551889f04b948b18f29aae560f79
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-09-06 01:11:07 +0800

    change loss to binary crossentropy and metric to meanIOU, added visualization

--- tiny_model.py
+++ tiny_model.py
@@ -1,7 +1,92 @@
 import tensorflow as tf
 
 from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
 
 class TinyUnet(object):
-    def __init__(self):
+    def __init__(self, input_shape=(128,128,1), num_classes=2):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
 
+    def get_crop_layer(self, src, target):
+        src_w = src.shape[2]
+        target_w = target.shape[2]
+
+        src_h = src.shape[1]
+        target_h = target.shape[1]
+
+        crop_h = int((src_h - target_h)/2)
+
+    def get_model(self):
+        inputs = Input(shape=self.input_shape) ### 128 x 128 x 1 ###
+
+        ''' Contracting path '''
+        ### 128 x 128 x 8 ###
+        conv_1 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(inputs)
+        ### 128 x 128 x 8 ###
+        conv_1 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(conv_1)
+        ### 64 x 64 x 8 ###
+        pool_1 = MaxPooling2D(pool_size=(2,2))(conv_1)
+
+        ### 64 x 64 x 16 ###
+        conv_2 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(pool_1)
+        ### 64 x 64 x 16 ###
+        conv_2 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(conv_2)
+        ### 32 x 32 x 16 ###
+        pool_2 = MaxPooling2D(pool_size=(2,2))(conv_2)
+
+        ### 32 x 32 x 32 ###
+        conv_3 = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(pool_2)
+        ### 32 x 32 x 32 ###
+        conv_3 = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(conv_3)
+        ### 16 x 16 x 32 ###
+        pool_3 = MaxPooling2D(pool_size=(2,2))(conv_3)
+
+        ### 16 x 16 x 64 ###
+        conv_4 = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same')(pool_3)
+        ### 16 x 16 x 64 ###
+        conv_4 = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same')(conv_4)
+        ### 8 x 8 x 64 ###
+        pool_4 = MaxPooling2D(pool_size=(2,2))(conv_4)
+
+        ### 8 x 8 x 128 ###
+        conv_5 = Conv2D(128, kernel_size=(3,3), activation='relu', padding='same')(pool_4)
+        ### 8 x 8 x 128 ###
+        conv_5 = Conv2D(128, kernel_size=(3,3), activation='relu', padding='same')(conv_5)
+
+        ''' Expansive path '''
+        ### 16 x 16 x 64 ###
+        up_1 = Conv2DTranspose(64, kernel_size=(2,2), strides=(2,2), padding='same')(conv_5)
+        ### 16 x 16 x 128 ###
+        concat_1 = tf.keras.layers.concatenate((conv_4, up_1), axis=3)
+        ### 16 x 16 x 64 ###
+        conv_6 = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same')(concat_1)
+        conv_6 = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same')(conv_6)
+
+        ### 32 x 32 x 32 ###
+        up_2 = Conv2DTranspose(32, kernel_size=(2,2), strides=(2,2), padding='same')(conv_6)
+        ### 32 x 32 x 64 ###
+        concat_2 = tf.keras.layers.concatenate((conv_3, up_2), axis=3)
+        ### 32 x 32 x 32 ###
+        conv_7 = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(concat_2)
+        conv_7 = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(conv_7)
+
+        ### 64 x 64 x 16 ###
+        up_3 = Conv2DTranspose(16, kernel_size=(2,2), strides=(2,2), padding='same')(conv_7)
+        ### 64 x 64 x 32 ###
+        concat_3 = tf.keras.layers.concatenate((conv_2, up_3), axis=3)
+        ### 64 x 64 x 16 ###
+        conv_8 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(concat_3)
+        conv_8 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(conv_8)
+
+        ### 128 x 128 x 8 ###
+        up_4 = Conv2DTranspose(8, kernel_size=(3,3), strides=(2,2), padding='same')(conv_8)
+        ### 128 x 128 x 16 ###
+        concat_4 = tf.keras.layers.concatenate((conv_1, up_4), axis=3)
+        ### 128 x 128 x 8 ###
+        conv_9 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(concat_4)
+        conv_9 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(conv_9)
+
+        outputs = Conv2D(self.num_classes, kernel_size=(1,1), activation='sigmoid')(conv_9)
+
+        return Model(inputs, outputs)
--- train.py
+++ train.py
@@ -3,19 +3,33 @@ import cv2
 import glob
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from model import Unet
+from tiny_model import TinyUnet
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras import backend as K
 
-net = Unet()
-model = net.get_model(num_classes=3)
+net = TinyUnet(num_classes=2)
+model = net.get_model()
+fig, ax = plt.subplots(3,3, figsize=(15,15))
 
-NUM_TRAIN_IMG=1
+testing_images = ['000e218f21.png', '2c707479f9.png', '589e94265a.png']
+
+NUM_TRAIN_IMG=100
+BATCH_SIZE=16
+EPOCHS=100 
 DATA_DIR = 'data/images'
 LABEL_DIR = 'data/masks'
-INPUT_SHAPE = (572, 572, 1)
-OUTPUT_SHAPE = (388,388, 1)
+MODEL_CHECKPOINT = 'checkpoints/model.weights.hdf5'
+INPUT_SHAPE = (128, 128, 1)
+OUTPUT_SHAPE = (128,128, 2)
+
+if(os.path.exists(MODEL_CHECKPOINT)):
+    print('[INFO] Transfer learning from old checkpoints ... ')
+    model.load_weights(MODEL_CHECKPOINT)
 
 print(model.summary())
 
@@ -39,18 +53,49 @@ for (dir, dirs, files) in os.walk(DATA_DIR):
         label_img = cv2.resize(label_img, (OUTPUT_SHAPE[0], OUTPUT_SHAPE[1]))
         label_img[label_img > 0] = 1
 
-        if(i == 0):
-            cv2.imshow('Sample input', img)
-            cv2.imshow('Sample output', label_img)
-            key = cv2.waitKey(0)
-
         train_img.append(img)
         train_labels.append(label_img)
         print('[INFO] Reading file : ' + abs_path)
         print('[INFO]   Label path : %s ' % label_id)
 
 train_img = np.array(train_img)
-train_labels = np.array(train_labels)
+train_labels = tf.one_hot(np.array(train_labels), depth=2)
+mean_iou = tf.keras.metrics.MeanIoU(num_classes=2)
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_img, train_labels, epochs=10)
+callbacks = [
+    EarlyStopping(patience=5, verbose=1),
+    ModelCheckpoint(MODEL_CHECKPOINT, verbose=1, save_best_only=True)
+]
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+model.fit(train_img, train_labels, epochs=EPOCHS, validation_split=0.1, batch_size=BATCH_SIZE, callbacks=callbacks)
+
+
+
+for i, img in enumerate(testing_images):
+    img_path = DATA_DIR + '/' + img
+    label_path = LABEL_DIR + '/' + img
+
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (INPUT_SHAPE[0], INPUT_SHAPE[1]))
+
+    label = cv2.imread(label_path)
+    label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
+    label = cv2.resize(label, (OUTPUT_SHAPE[0], OUTPUT_SHAPE[1]))
+
+    prediction = model.predict(np.array([img]))[0]
+    prediction = (prediction > 0.5).astype(np.uint8)[:,:,1]
+    print(prediction)
+
+    ax[i][0].imshow(img)
+    ax[i][0].set_title("Input")
+
+    ax[i][1].imshow(label)
+    ax[i][1].set_title("Ground Truth")
+
+    ax[i][2].imshow(prediction)
+    ax[i][2].set_title("Prediction")
+
+plt.show()
